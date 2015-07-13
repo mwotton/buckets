c4aae3f1b729a5bed202501ae57896114c7bc378
Mark Wotton <mwotton@gmail.com>
2015-07-13 15:53:14 -0400

    generalise to arbitrary monads

--- src/Control/Buckets.hs
+++ src/Control/Buckets.hs
@@ -14,23 +14,25 @@ data Event k a = Entries !k !a
 --           t -> m (Event k a) -> (k -> [a] -> m ()) -> m ()
 -- bucket :: (Hashable k, Num t, Eq t, Eq k) => t -> IO (Event k a1) -> (k -> [a1] -> IO a) -> IO ()
 
-bucket :: (Hashable a1, Hashable k, Eq a1, Eq k) => Int -> IO (Event k [a1]) -> (k -> [[a1]] -> IO a) -> IO ()
+bucket :: (Monad m, Functor m, Hashable a1, Hashable k, Eq a1, Eq k)
+          => Int -> m (Event k [a1]) -> (k -> [[a1]] -> m a) -> m ()
 bucket maxEntries fetch dispose = go HM.empty
   where go dict = do
           res <- fetch
           case res of
-            Quit -> flush dict >> putStrLn "quitting"
-            Flush -> flush dict >> putStrLn "flush called" >> go HM.empty
+            Quit -> flush dict --  >>  "quitting"
+            Flush -> flush dict >>
+                     -- putStrLn "flush called" >>
+                     go HM.empty
             Entries tag entry -> do
               newDict <- case HM.lookup tag dict of
                 Nothing -> return $ HM.insert tag (maxEntries, HS.singleton entry) dict
                 Just (n,entries) ->
                   if n <= 0
-                  then do dispose tag (HS.toList entries)
+                  then do void $ dispose tag (HS.toList entries)
                           return (HM.insert tag (maxEntries,HS.empty) dict)
                   else return $ HM.insert tag (n-(length entry),(HS.insert entry entries)) dict
               go newDict
 
         flush dict = do
           void $ sequence $ HM.foldlWithKey' (\actions k (_,v) -> (dispose k (HS.toList v)):actions) [] dict
-          print "flushed"
